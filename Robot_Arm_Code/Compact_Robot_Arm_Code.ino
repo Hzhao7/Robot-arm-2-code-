b00204487d1cbb4d89926c25a436a06fc880a62f
Hzhao7 <154693760+Hzhao7@users.noreply.github.com>
2025-05-26 07:22:12 -0400

    Update Compact_Robot_Arm_Code.ino
    included lcd display and fixed some errors

--- Robot_Arm_Code/Compact_Robot_Arm_Code.ino
+++ Robot_Arm_Code/Compact_Robot_Arm_Code.ino
@@ -1,72 +1,90 @@
 #include <Wire.h>
  
 #include <Adafruit_PWMServoDriver.h>
+#include <LiquidCrystal.h>
 
 #define MIN_PULSE_WIDTH       150
 #define MAX_PULSE_WIDTH       725
 #define FREQUENCY             50
  
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+LiquidCrystal lcd(6, 7, 2, 3, 4, 5);
 
-int potWrist = A3;
-int potElbow = A2;                        //Assign Potentiometers to pins on Arduino Uno
-int potShoulder = A1;
-int potBase = A0;
+int potWrist = A0;
+int potElbow = A1;                        //Assign Potentiometers to pins on Arduino Uno
+int potShoulder = A2;
+int potBase = A3;
 
-int hand = 11;
+int gripper = 11;
 int wrist = 12;
 int elbow = 13;                           //Assign Motors to pins on Servo Driver Board
 int shoulder = 14;
 int base = 15;
 
-void setup() 
+void resetMotorPos()
 {
-  delay(5000);                            // <-- So I have time to get controller to starting position
-  
+  pwm.setPWM(12, 0, 410);
+  delay(1000);
+  pwm.setPWM(13, 0, 725);
+  delay(1000);
+  pwm.setPWM(14, 0, 150);
+  delay(1000);
+  pwm.setPWM(15, 0, 150);
+  delay(1000);
+}
+
+void setup() 
+{ 
+  lcd.begin(16, 2);
   pwm.begin();
   pwm.setPWMFreq(FREQUENCY);
-  pwm.setPWM(11, 0, 90);                  //Set Gripper to 90 degrees (Close Gripper)
+  pwm.setPWM(11, 0, 150);                  //Set Gripper to 90 degrees (Close Gripper)
   
   pinMode(13,INPUT_PULLUP);
-  
+  pinMode(12,INPUT_PULLUP);
+  resetMotorPos();
   Serial.begin(9600);
 }
  
  
-void moveMotor(int controlIn, int motorOut)
+void moveMotor(int controlIn, int motorOut) 
 {
   int pulse_wide, pulse_width, potVal;
   
   potVal = analogRead(controlIn);                                                   //Read value of Potentiometer
   
-  pulse_wide = map(potVal, 800, 240, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
-  pulse_width = int(float(pulse_wide) / 1000000 * FREQUENCY * 4096);                //Map Potentiometer position to Motor
+  pulse_wide = map(potVal, 1013, 10, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH); //accounts for if it somehow gets tied to high or low so the arm will not swing
+  if (controlIn == A1){
+    pulse_wide = map(potVal, 1013, 10, MAX_PULSE_WIDTH, MIN_PULSE_WIDTH);
+  }
   
-  pwm.setPWM(motorOut, 0, pulse_width);
+  pwm.setPWM(motorOut, 0, pulse_wide);
  
 }
+
  
-void loop() 
+void loop()
 {
   moveMotor(potWrist, wrist);
-  
   moveMotor(potElbow, elbow);
-                                                            //Assign Motors to corresponding Potentiometers
   moveMotor(potShoulder, shoulder);
-  
   moveMotor(potBase, base);
 
-
-
-  int pushButton = digitalRead(13);
-  if(pushButton == LOW)
+  int gripperButton = digitalRead(13);
+  if (gripperButton == HIGH)
   {
-    pwm.setPWM(hand, 0, 150);                             //Keep Gripper closed when button is not pressed
-    Serial.println("Grab");
+    pwm.setPWM(gripper, 0, 150);                             //Keep Gripper closed when button is not pressed
+    lcd.clear();
   }
   else
   {
-    pwm.setPWM(hand, 0, 270);                              //Open Gripper when button is pressed
-    Serial.println("Release");
+    pwm.setPWM(gripper, 0, 225);                              //Open Gripper when button is pressed
+    lcd.print("grab");
   }
+
+  int resetButton = digitalRead(12);
+  if (resetButton == LOW){
+    resetMotorPos();
+  }
+
 }
