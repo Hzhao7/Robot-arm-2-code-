d56e5a5c7057b4656591b4389373e2c14a3b9910
Hzhao7 <154693760+Hzhao7@users.noreply.github.com>
2025-05-26 23:00:17 -0400

    Update Compact_Robot_Arm_Code.ino
    included the base for torque calculations

--- Robot_Arm_Code/Compact_Robot_Arm_Code.ino
+++ Robot_Arm_Code/Compact_Robot_Arm_Code.ino
@@ -21,8 +21,10 @@ int elbow = 13;                           //Assign Motors to pins on Servo Drive
 int shoulder = 14;
 int base = 15;
 
-void resetMotorPos()
-{
+double motorOneTorque = 0;                   // torque in Nm
+double motorTwoTorque = 0;
+
+void resetMotorPos(){
   pwm.setPWM(12, 0, 410);
   delay(1000);
   pwm.setPWM(13, 0, 725);
@@ -33,9 +35,14 @@ void resetMotorPos()
   delay(1000);
 }
 
-void setup() 
-{ 
+void setup() { 
   lcd.begin(16, 2);
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print(String("Torque 1: ") + String(motorOneTorque)); // initialize the LCD
+  lcd.setCursor(0, 2);
+  lcd.print(String("Torque 2: ") + String(motorTwoTorque));
+
   pwm.begin();
   pwm.setPWMFreq(FREQUENCY);
   pwm.setPWM(11, 0, 150);                  //Set Gripper to 90 degrees (Close Gripper)
@@ -47,8 +54,7 @@ void setup()
 }
  
  
-void moveMotor(int controlIn, int motorOut) 
-{
+void moveMotor(int controlIn, int motorOut) {
   int pulse_wide, pulse_width, potVal;
   
   potVal = analogRead(controlIn);                                                   //Read value of Potentiometer
@@ -62,29 +68,44 @@ void moveMotor(int controlIn, int motorOut)
  
 }
 
+void calculateTorques(int mass){
+  float L1 = 0.125; // known values from measurements
+  float L2 = 0.09;
+  float m1 = 0.101;
+  float m2 = 0.111;
+
+  int elbowVal, elbowAngle;
+  
+  elbowVal = analogRead(A1);
+  elbowAngle = map(elbowVal, 1013, 10, 0, 2*3.14159);
+  
+}
+
  
-void loop()
-{
+void loop(){
   moveMotor(potWrist, wrist);
   moveMotor(potElbow, elbow);
   moveMotor(potShoulder, shoulder);
   moveMotor(potBase, base);
 
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print(String("Torque 1: ") + String(motorOneTorque)); // updates LCD
+  lcd.setCursor(0, 2);
+  lcd.print(String("Torque 2: ") + String(motorTwoTorque));
+
   int gripperButton = digitalRead(13);
   if (gripperButton == HIGH)
   {
     pwm.setPWM(gripper, 0, 150);                             //Keep Gripper closed when button is not pressed
-    lcd.clear();
   }
   else
   {
     pwm.setPWM(gripper, 0, 225);                              //Open Gripper when button is pressed
-    lcd.print("grab");
   }
 
-  int resetButton = digitalRead(12);
+  int resetButton = digitalRead(12);                          // reset button to go back to start position
   if (resetButton == LOW){
     resetMotorPos();
   }
-
 }
