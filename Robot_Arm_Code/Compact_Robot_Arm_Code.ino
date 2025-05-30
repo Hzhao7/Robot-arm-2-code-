c871a9b141044f6441b8122ecc09cfb8ac8b347d
Hzhao7 <154693760+Hzhao7@users.noreply.github.com>
2025-05-29 21:10:03 -0400

    Update Compact_Robot_Arm_Code.ino
    Finished the calculate torque function and improved the reset fucntion

--- Robot_Arm_Code/Compact_Robot_Arm_Code.ino
+++ Robot_Arm_Code/Compact_Robot_Arm_Code.ino
@@ -1,4 +1,5 @@
 #include <Wire.h>
+#define PI 3.1415926535897932384626433832795
  
 #include <Adafruit_PWMServoDriver.h>
 #include <LiquidCrystal.h>
@@ -25,9 +26,9 @@ double motorOneTorque = 0;                   // torque in Nm
 double motorTwoTorque = 0;
 
 void resetMotorPos(){
-  pwm.setPWM(12, 0, 410);
+  pwm.setPWM(12, 0, 600);
   delay(1000);
-  pwm.setPWM(13, 0, 725);
+  pwm.setPWM(13, 0, 600);
   delay(1000);
   pwm.setPWM(14, 0, 150);
   delay(1000);
@@ -61,26 +62,86 @@ void moveMotor(int controlIn, int motorOut) {
   
   pulse_wide = map(potVal, 1013, 10, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH); //accounts for if it somehow gets tied to high or low so the arm will not swing
   if (controlIn == A1){
-    pulse_wide = map(potVal, 1013, 10, MAX_PULSE_WIDTH, MIN_PULSE_WIDTH);
+    pulse_wide = map(potVal, 1013, 10, MAX_PULSE_WIDTH, MIN_PULSE_WIDTH); //the elbow direction is reversed as there was an issue in the build process thaty could not be altered
   }
   
   pwm.setPWM(motorOut, 0, pulse_wide);
  
 }
 
-void calculateTorques(int mass){
-  float L1 = 0.125; // known values from measurements
-  float L2 = 0.09;
-  float m1 = 0.101;
-  float m2 = 0.111;
+void calculateTorques(float mass){ // function to calcuate the torque values
+  const float L1 = 0.125; // known values from measurements
+  const float L2 = 0.09;
+  const float m1 = 0.055;
+  const float m2 = 0.057;
+  const float motor1 = 0.055;
+  const float g = 9.80665;
+  float m3 = 0.04 + mass; 
 
-  int elbowVal, elbowAngle;
+  float elbowVal, theata1, shoulderVal, theata2;
   
+  // maps the potentiometer values to angles theata1 and theata 2
   elbowVal = analogRead(A1);
-  elbowAngle = map(elbowVal, 1013, 10, 0, 2*3.14159);
+  theata2 = map(elbowVal, 740, 10, 0, 180);
+  shoulderVal = analogRead(A2);
+  theata1 = map(shoulderVal, 1013, 300, 0, 180); //tried with radians but it would only interva whole numbers making it less accurate
+  /*Serial.println(theata1);
+  Serial.println(elbowVal);
+  Serial.println(theata2);
+  Serial.println(shoulderVal);
+  delay(1000);*/ // Calibration code
+  if (elbowVal > 740){ // gets rid of values outside of the domain
+    theata2 = 0;
+  }
+  if (elbowVal < 10){
+    theata2 = PI;
+  }
+  if (shoulderVal < 300){
+    theata1 = 180;
+  }
+  if (shoulderVal > 1013){
+    theata1 = 0;
+  }
+  theata1 = theata1*PI/180;
+  theata2 = theata2*PI/180;
+  motorOneTorque = L2*sin(PI/2 - theata2 + theata1)*(m2*g/2 + m3*g);
   
-}
+  // finds what equation to use for motorTwoTorque
+  float positionCase = 0;
+  float L3 = sqrt(L1*L1 + (L2/2)*(L2/2) - L1*L2*cos(theata2));
+  float L4 = sqrt(L1*L1 + L2*L2 - 2*L1*L2*cos(theata2));
+  float theata4 = asin(L1/L3*sin(theata2));
+  float theata5 = asin(L1/L4*sin(theata2));
 
+  /* if (theata2 > theata1 && theata1 <= 1.71 && theata2 <= 1.71){
+    positionCase = L3*m2*g*sin(theata4 - PI/2 + theata2 - theata1) - L4*m3*g*sin(PI/2 - theata5 + theata2 - theata1);
+  }
+  if (theata2 > theata1 && theata1 > 1.71 && theata2 > 1.71){
+    positionCase = L3*m2*g*sin(PI/2 - theata4 - theata2 + theata1) + L4*m3*g*sin(PI/2 - theata5 - theata2 + theata1);
+  }
+  if (theata2 < theata1 && theata1 > 1.71 && theata2 <= 1.71){
+    positionCase = L3*m2*g*sin(PI/2 - theata4 + theata1 - theata2) + L4*m3*g*sin(PI/2 - theata5 + theata1 - theata2);
+  }
+  else{
+    positionCase = 0;
+  }
+  */ //code for trying to account for each case
+  if (theata1 > 1.71){
+    positionCase = L3*m2*g*sin(PI/2 - theata4 + theata1 - theata2) + L4*m3*g*sin(PI/2 - theata5 + theata1 - theata2);
+  }
+  else{
+    positionCase = 0;
+  }
+
+  motorTwoTorque = L1*sin(PI/2 - theata1)*(m1*g/2 + motor1*g) + positionCase;
+
+  if (elbowVal > 740){ // Accounts for when the elbow is resting on the shoulder and the motor is not moving the elbow
+    motorOneTorque = 0;
+  }
+  if (shoulderVal > 1013){ // Accounts for when the shoulder is at rest
+    motorTwoTorque = 0;
+  }
+}
  
 void loop(){
   moveMotor(potWrist, wrist);
@@ -89,18 +150,17 @@ void loop(){
   moveMotor(potBase, base);
 
   lcd.clear();
-  lcd.setCursor(0, 1);
+  calculateTorques(1); // calculates the new torques
+  lcd.setCursor(0, 0);
   lcd.print(String("Torque 1: ") + String(motorOneTorque)); // updates LCD
-  lcd.setCursor(0, 2);
+  lcd.setCursor(0, 1);
   lcd.print(String("Torque 2: ") + String(motorTwoTorque));
 
   int gripperButton = digitalRead(13);
-  if (gripperButton == HIGH)
-  {
+  if (gripperButton == HIGH){
     pwm.setPWM(gripper, 0, 150);                             //Keep Gripper closed when button is not pressed
   }
-  else
-  {
+  else{
     pwm.setPWM(gripper, 0, 225);                              //Open Gripper when button is pressed
   }
 
